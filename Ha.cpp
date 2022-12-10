ad1b48e666d62e80555fc60e4e2f259838054d35
grinara <114216399+grinara@users.noreply.github.com>
2022-12-10 22:46:32 +0700

    Update Ha.cpp

--- Ha.cpp
+++ Ha.cpp
@@ -1,34 +1,17 @@
+#include "Header.h"
 #include <iostream>
 #include <vector>
 #include <map>
 #include <list>
 #include <fstream>
 #include <string>
-#include "Source.h"
 using namespace std;
-class Uzel {
-public:
-	int a;
-	char c;
-	Uzel* left, * right;
-	Uzel() {
-		left = right = NULL;
-	}
-	Uzel(Uzel* L, Uzel* R)
-	{
-		left = L;
-		right = R;
-		a = L->a + R->a;
-	}
 
-};
 struct MyCompare
 {
 	bool operator()(const Uzel* l, const Uzel* r) const { return l->a < r->a; }
 };
-
-
-void Print(Uzel* root, unsigned k = 0) {
+void Print(Uzel* root, unsigned k=0) {
 	if (root != NULL) {
 		Print(root->left, k + 3);
 		for (unsigned i = 0; i < k; i++) {
@@ -72,11 +55,10 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	ifstream file1("11.txt", ios::out | ios::binary);
 	map<char, int>  m;
-	while(!file1.eof()) { //ищём количество вхождений каждого символа
+	while (!file1.eof()) { //ищём количество вхождений каждого символа
 		char c = file1.get();
-			m[c]++;
+		m[c]++;
 	}
-
 	list<Uzel*> da;
 
 	map<char, int>::iterator i;
@@ -100,42 +82,37 @@ int main() {
 		da.push_back(parent); // добавляем его в list
 	}
 	Uzel* root = da.front(); // указатель на корень дерева
-	Print(root);
+	//Print(root);
 	Build(root);
 
 	file1.clear(); // сбрасываем указатель
 	file1.seekg(0);// возвращаем указатель в начало файла
 
-	ofstream file1_cip("output.txt", ios::out | ios::binary); // файл для записи
+	fstream file1_cip("112.txt", ios::binary | ios::out);
+	int c = m.size();
+	char buff = 0;
+	file1_cip.write((char*)&c, sizeof(int));
+	for (i = m.begin(); i != m.end(); ++i) {
+		buff = i->first;
+		c = i->second;
+		file1_cip.write((char*)&buff, sizeof(char));
+		file1_cip.write((char*)&c, sizeof(int));
+	}
 
 	int count = 0; // счётчик
 	char buf = 0; //вспомогательная переменная
-	while(!file1.eof()) {
+	while (!file1.eof()) {
 		char c = file1.get();
-			vector<bool> x = table[c];
-			for (int j = 0; j < x.size(); j++) {
-				buf = buf | x[j] << (7 - count); // преобразуем вектор x в байт
-				count++;
-				if (count == 8) { count = 0; file1_cip << buf; buf = 0; } // если достигли count = 8  байт записывем в file1_cip
-			}
+		vector<bool> x = table[c];
+		for (int j = 0; j < x.size(); j++) {
+			buf = buf | x[j] << (7 - count); // преобразуем вектор x в байт
+			count++;
+			if (count == 8) { count = 0; file1_cip << buf; buf = 0; } // если достигли count = 8  байт записывем в file1_cip
+		}
 	}
 
 	file1.close(); // не забываем закрыть файл
 	file1_cip.close(); // не забываем закрыть файл
-	ifstream F("output.txt", ios::in | ios::binary);
-	Uzel* p = root;
-	count = 0;
-	char byte=0;
-	byte = F.get();
-	while (!F.eof())
-	{
-		bool b = byte & (1 << (7 - count)); // проверяем 1 или 0 в кодировке
-		if (b) p = p->right; else p = p->left; // если 1 двигаемся вправо иначе влево
-		if ((p->left == NULL) && (p->right == NULL)) { cout << p->c; p = root; } // выводим символ 
-		count++;
-		if (count == 8) { count = 0; byte = F.get(); } // если cout = 8 обнуляем его и рассматриваем следующий символ
-	}
-
-	F.close(); // не забываем закрыть файл
+	main1();
 	return 0;
 }
